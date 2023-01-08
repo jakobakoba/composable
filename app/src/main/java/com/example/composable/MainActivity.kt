f53bfd948ca2641590ed89de0d1d2d7252c4aa6f
jakobakoba <jakobakoba@gmail.com>
2023-01-08 17:50:08 +0600

    rowdies font added

--- app/src/main/java/com/example/composable/MainActivity.kt
+++ app/src/main/java/com/example/composable/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composable
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,21 +18,28 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.time.temporal.TemporalQueries.offset
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
 
-            val source = painterResource(id = R.drawable.bluewhite)
-            val title = "Balloons"
-            val description = "Blue and white balloons "
-            Box(modifier=Modifier.fillMaxWidth(0.5f)){
-                ImageCard(source = source, title = title , description = description)
-            }
+                jctext()
+
             }
 
         }
@@ -46,7 +54,9 @@ fun ImageCard(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier.fillMaxWidth().padding(10.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp,
 
@@ -60,13 +70,13 @@ fun ImageCard(
             Box(modifier = Modifier
                 .fillMaxSize()
                 .background(
-                Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent,
-                        Color.Black
-                    ),
-                    startY = 300f
-                )
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
                 )
             )
             Box(modifier = Modifier
@@ -79,4 +89,54 @@ fun ImageCard(
         }
     }
 
+}
+
+@Composable
+fun jctext(){
+    val fontFamily = FontFamily(
+        Font(R.font.rowdies_bold, FontWeight.Bold),
+        Font(R.font.rowdies_light, FontWeight.Light),
+        Font(R.font.rowdies_regular, FontWeight.Normal)
+    )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black),
+//                contentAlignment = Alignment.Center,
+
+
+    ){
+        Text(
+
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize  = 50.sp
+                    )
+                ){
+                    append("J")
+                }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize  = 50.sp
+                    )
+                ){
+                    append("C")
+                }
+                append("ompose")
+            },
+
+            color = Color.White,
+            fontSize = 35.sp,
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Right,
+            modifier = Modifier.offset(y=200.dp),
+            textDecoration = TextDecoration.Underline
+        )
+
+    }
 }
