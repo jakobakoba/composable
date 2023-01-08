42eefca4834632395ebb469378dba5f7bfc69c48
jakobakoba <jakobakoba@gmail.com>
2023-01-08 16:34:17 +0600

    Image Card added

--- app/src/main/java/com/example/composable/MainActivity.kt
+++ app/src/main/java/com/example/composable/MainActivity.kt
@@ -3,41 +3,80 @@ package com.example.composable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composable.ui.theme.ComposableTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposableTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+            val source = painterResource(id = R.drawable.bluewhite)
+            val title = "Balloons"
+            val description = "Blue and white balloons "
+            Box(modifier=Modifier.fillMaxWidth(0.5f)){
+                ImageCard(source = source, title = title , description = description)
+            }
+            }
+
+        }
+    }
+
+
+@Composable
+fun ImageCard(
+    source : Painter,
+    title : String,
+    description : String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth().padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
+
+    ) {
+        Box(modifier = Modifier.height(250.dp)){
+            Image(
+                painter = source,
+                contentDescription = description,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 300f
+                )
+                )
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart
+            ){
+                Text(title, style = TextStyle(color = Color.White, fontSize = 12.sp))
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposableTheme {
-        Greeting("Android")
-    }
 }
